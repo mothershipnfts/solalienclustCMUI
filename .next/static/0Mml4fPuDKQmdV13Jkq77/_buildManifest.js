e4025be87136de0c91ca7daa67c3aa0a2f301b69
mothershipnfts <mothershipnfts@gmail.com>
2024-08-02 02:53:46 -0700

    first commit

--- .next/static/0Mml4fPuDKQmdV13Jkq77/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/344-9cc06a1f621ec032.js","static/chunks/pages/index-90c24824de26d471.js"],"/_error":["static/chunks/pages/_error-9c7a64dc41a16fc5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
