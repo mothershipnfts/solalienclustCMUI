7635b95517163b02600d41ae33c3bb9ae421035d
Brendan <mothershipnfts@gmail.com>
2024-08-09 14:30:57 -0700

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -16,35 +16,33 @@ export const HomeView: FC = ({ }) => {
   const wallet = useWallet();
   const { connection } = useConnection();
 
-  const balance = useUserSOLBalanceStore((s) => s.balance)
-  const { getUserSOLBalance } = useUserSOLBalanceStore()
+  const balance = useUserSOLBalanceStore((s) => s.balance);
+  const { getUserSOLBalance } = useUserSOLBalanceStore();
 
   useEffect(() => {
     if (wallet.publicKey) {
-      console.log(wallet.publicKey.toBase58())
-      getUserSOLBalance(wallet.publicKey, connection)
+      console.log(wallet.publicKey.toBase58());
+      getUserSOLBalance(wallet.publicKey, connection);
     }
-  }, [wallet.publicKey, connection, getUserSOLBalance])
+  }, [wallet.publicKey, connection, getUserSOLBalance]);
 
   return (
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
-        <div className='mt-0'>
-          
-        </div>
+        <div className='mt-0'></div>
         
         <div className="relative group">
-  <div className="relative z-10">
-    <Image
-      src="/solalienclusterlogo2.png" // Replace with your image path or URL
-      alt="Sol Alien Cluster"
-      layout="responsive"
-      width={800} // Set appropriate width
-      height={600} // Set appropriate height
-      className="object-contain relative z-10 glow-image" // Adjust the size as needed
-    />
-  </div>
-</div>
+          <div className="relative z-10">
+            <Image
+              src="/solalienclusterlogo2.png" // Replace with your image path or URL
+              alt="Sol Alien Cluster"
+              layout="responsive"
+              width={800} // Set appropriate width
+              height={600} // Set appropriate height
+              className="object-contain relative z-10 glow-image" // Adjust the size as needed
+            />
+          </div>
+        </div>
 
         <div className="flex flex-col mt-2">
           <CandyMint /> 
@@ -52,9 +50,8 @@ export const HomeView: FC = ({ }) => {
             {wallet && (
               <div className="flex flex-col items-center">
                 <div className='text-center text-slate-420 ml-2'>
-                Welcome to the Sol Alien Cluster—a group of aliens chilling on the Solana Network.
+                  Welcome to the Sol Alien Cluster—a group of aliens chilling on the Solana Network.
                   Before there were chimps, there were aliens. Now it&#39;s time for the aliens to return and take over the world. It&#39;s time to destroy all humans.
-                </div>
                 </div>
               </div>
             )}
@@ -68,9 +65,9 @@ export const HomeView: FC = ({ }) => {
             <div className="flex-1">
               <h2 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-green-600 mb-10">Uncompromised style</h2>
               <p className="text-lg text-gray-400">
-  The aliens have learned one thing from humans: we have style. To blend in and disguise themselves as they prepare to take over the world again, the aliens are now dressed like humans.
+                The aliens have learned one thing from humans: we have style. To blend in and disguise themselves as they prepare to take over the world again, the aliens are now dressed like humans.
                 In due time, you will see the aliens rule the world once more. When that happens, you&#39;d better be holding a Sol Alien Cluster—otherwise, you may be incinerated.
-</p>
+              </p>
             </div>
 
             {/* GIF on the Right */}
@@ -111,8 +108,9 @@ export const HomeView: FC = ({ }) => {
             </div>
           </div>
         </div>
-      {/* Additional Mint Button */}
-      <div className="flex justify-center mt-16">
+
+        {/* Additional Mint Button */}
+        <div className="flex justify-center mt-16">
           <CandyMint />
         </div>
  
