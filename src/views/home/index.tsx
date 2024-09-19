df70b1f59008c753b2ee7760f7291e428e8409ee
Brendan <mothershipnfts@gmail.com>
2024-09-18 19:59:15 -0700

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -34,7 +34,7 @@ export const HomeView: FC = ({ }) => {
         <div className="relative group">
           <div className="relative z-10">
             <Image
-              src="/solaliencluster-logo.png" // Replace with your image path or URL
+              src="/solalienclusterlogo2.png" // Replace with your image path or URL
               alt="Sol Alien Cluster"
               layout="responsive"
               width={800} // Set appropriate width
